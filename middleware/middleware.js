b9ce68ef3f017a5730582ab5b454dfdf09d41bb1
Chao Liu <liuchao8989@gmail.com>
2016-11-14 22:27:31 -0800

    indentation for middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,31 +1,31 @@
 var Campground = require("../models/campground");
 
-module.exports ={
-  isLoggedIn: function (req, res, next){
+module.exports = {
+  isLoggedIn: function(req, res, next) {
     console.log("this is isLoggedIn");
-    if(req.isAuthenticated()){
-        return next();
+    if (req.isAuthenticated()) {
+      return next();
     }
     res.redirect("/login");
   },
 
-  isCampgroundOwner: function (req,res,next){
-    if(req.isAuthenticated()){
+  isCampgroundOwner: function(req, res, next) {
+    if (req.isAuthenticated()) {
       console.log("is auth");
-      Campground.findById(req.params.id, function(e,campground){
-        if(e){
-          console.log("middleware is isSameAuthor err: "+e);
-           res.redirect("/campgrounds")
-        } else{
-          if(req.user._id === campground.author.id){
-             next();      
+      Campground.findById(req.params.id, function(e, campground) {
+        if (e) {
+          console.log("middleware is isSameAuthor err: " + e);
+          res.redirect("/campgrounds")
+        } else {
+          if (req.user._id === campground.author.id) {
+            next();
           } else {
-             res.redirect("/campgrounds");
+            res.redirect("/campgrounds");
           }
         }
       });
-    }else {
-      res.redirect("/login");  
+    } else {
+      res.redirect("/login");
     }
   }
 }
